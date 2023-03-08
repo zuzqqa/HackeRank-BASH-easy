6ce10f4f4c0adbac7332566adc3f1ef23870f5fc
zuzqqa <zuzannanowak04@wp.pl>
2023-03-08 20:50:53 +0100

    Create Comparing Numbers.sh

---
+++ Comparing Numbers.sh
@@ -0,0 +1,12 @@
+ifread X
+read Y
+if [ "$X" -lt "$Y" ]
+then 
+    echo "X is less than Y"
+elif [ "$X" -gt "$Y" ]
+then
+    echo "X is greater than Y"
+elif [ "$X" -eq "$Y" ]
+then 
+    echo "X is equal to Y"
+fi
